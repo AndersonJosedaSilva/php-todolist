1d578ebf78aa91d3e3ee645e1e85c0352c890f88
andersonjose <anderson.pan@hotmail.com>
2015-11-16 22:30:51 +0000

    php

--- api/database/ConnectionFactory.php
+++ api/database/ConnectionFactory.php
@@ -9,6 +9,13 @@ class ConnectionFactory{
     
     private static function getConnection(){
          $dbhost = getenv('IP');
+         $dbuser = getenv('C9_USER');
+         $dbpass = '';
+         $dbname = 'C9';
+         
+         $connection = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbpass,$dbuser);
+         return $connection;
+         
         
     }
 }
